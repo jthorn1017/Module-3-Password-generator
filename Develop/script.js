b3dfc99deeb03dd74da5d3868559980b83f4eec1
Jay Thornton <jaythornton@Jays-MacBook-Air.local>
2023-02-26 19:12:21 -0500

    new repo

---
+++ Develop/script.js
@@ -0,0 +1,29 @@
+// Assignment code here
+function generatePassword() {
+    var passwordLength = prompt("How many characters would you like your password to be?")
+    console.log("Password length", passwordLength)
+    var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ]
+    var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    var nonCapital = prompt("Do you want to include lower case letters?")
+    var Capital = prompt("Do you want to include upper case letters?")
+    var numbers = prompt("Do you want to include numbers?")
+    var specialCharacters = prompt("Do you want to include special characters?")
+    return "Test"
+  }
+  
+  
+  // Get references to the #generate element
+  var generateBtn = document.querySelector("#generate");
+  
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+  
+  }
+  
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+  
